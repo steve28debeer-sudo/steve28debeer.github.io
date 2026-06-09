812b9a0a3e30d0e610099bf110f9952368bbdeef
steve28debeer-sudo <steve28debeer@gmail.com>
2026-06-09 23:51:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let missingFruit = "";
+
 function showFruit(name, emoji) {
   const display = document.getElementById("display");
 
@@ -6,4 +8,42 @@ function showFruit(name, emoji) {
     <h2>${name}</h2>
     <p>Say it with me: ${name}!</p>
   `;
+}
+
+function hideFruit() {
+  const cards = document.querySelectorAll(".fruit-card");
+
+  resetCards();
+
+  const updatedCards = document.querySelectorAll(".fruit-card");
+  const randomIndex = Math.floor(Math.random() * updatedCards.length);
+  const chosenCard = updatedCards[randomIndex];
+
+  missingFruit = chosenCard.textContent.trim();
+
+  chosenCard.innerHTML = "";
+  chosenCard.classList.add("hidden-card");
+
+  document.getElementById("questionText").textContent =
+    "Which fruit is missing?";
+}
+
+function resetCards() {
+  const fruits = [
+    "🍎<br>Apple",
+    "🍌<br>Banana",
+    "🍊<br>Orange",
+    "🍐<br>Pear",
+    "🥝<br>Kiwi"
+  ];
+
+  const cards = document.querySelectorAll(".fruit-card");
+
+  cards.forEach((card, index) => {
+    card.innerHTML = fruits[index];
+    card.classList.remove("hidden-card");
+  });
+
+  document.getElementById("questionText").textContent =
+    "Look carefully! Which fruit will disappear?";
 }
